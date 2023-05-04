f05d30ba5d1655acabc21b0208547ca2245b9f29
Abdullah <abdullahfazlani5@gmail.com>
2023-05-05 00:46:15 +0500

    feat/update-form

--- src/components/select/MultiSelect.js
+++ src/components/select/MultiSelect.js
@@ -19,16 +19,22 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
+  FormLabel,
   Input,
   InputAdornment,
   InputLabel,
+  Radio,
+  RadioGroup,
   Slider,
   TextField,
+  ToggleButton,
+  ToggleButtonGroup,
   Typography,
 } from '@mui/material';
 
 import FieldCheckbox from './section/FieldCheckbox';
 import Selection from './section/Selection';
+import RangeSlider from './section/Slider';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -53,7 +59,6 @@ const names = [
   'Virginia Andrews',
   'Kelly Snyder',
 ];
-
 
 const checkbox_items = [
   'Oliver Hansen',
@@ -148,9 +153,14 @@ export default function MultipleSelect() {
   const handleChange3 = (event) => {
     setChecked([checked[0], event.target.checked]);
   };
+
+  const [alignment, setAlignment] = React.useState('web');
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
   return (
     <div style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <TextField
           label="Domain Search"
@@ -158,7 +168,7 @@ export default function MultipleSelect() {
           sx={{
             m: 1,
             backgroundColor: '#fff',
-            width: '20%',
+            width: '40%',
           }}
           InputProps={{
             startAdornment: (
@@ -170,7 +180,10 @@ export default function MultipleSelect() {
         />
       </div>
       <div>
-      
+        <RangeSlider />
+      </div>
+
+      <div style={{ border: '1px solid grey', borderRadius: '10px' }}>
         <Accordion sx={{ width: '100%', backgroundColor: '#fff' }}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
@@ -188,7 +201,7 @@ export default function MultipleSelect() {
           </AccordionDetails>
         </Accordion>
       </div>
-      <div>
+      <div style={{ border: '1px solid grey', borderRadius: '10px' }}>
         <Accordion sx={{ width: '100%', backgroundColor: '#fff' }}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
@@ -232,7 +245,7 @@ export default function MultipleSelect() {
           </AccordionDetails>
         </Accordion>
       </div>
-      <div>
+      <div style={{ border: '1px solid grey', borderRadius: '10px' }}>
         <Accordion sx={{ width: '100%', backgroundColor: '#fff' }}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
@@ -276,6 +289,156 @@ export default function MultipleSelect() {
           </AccordionDetails>
         </Accordion>
       </div>
+      <div style={{ border: '1px solid grey', borderRadius: '10px' }}>
+        <Accordion sx={{ width: '100%', backgroundColor: '#fff' }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Traffic Source Filter</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <div>
+                <FormControlLabel
+                  label="Select All"
+                  control={
+                    <Checkbox
+                      checked={checked[0] && checked[1]}
+                      indeterminate={checked[0] !== checked[1]}
+                      onChange={handleChange1}
+                    />
+                  }
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'row', ml: 3 }}>
+                  <FormControlLabel
+                    label="USA"
+                    control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
+                    sx={{ border: '1px solid gray', borderRadius: '20px', pr: '10px' }}
+                  />
+                  <FormControlLabel
+                    label="UK"
+                    control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+                    sx={{ border: '1px solid gray', borderRadius: '20px', pr: '10px' }}
+                  />
+                  <FormControlLabel
+                    label="India"
+                    control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+                    sx={{ border: '1px solid gray', borderRadius: '20px', pr: '10px' }}
+                  />
+                </Box>
+              </div>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+      <div style={{ border: '1px solid grey', borderRadius: '10px' }}>
+        <Accordion sx={{ width: '100%', backgroundColor: '#fff' }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Select Traffic Location</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <div>
+                <FormControlLabel
+                  label="Select All"
+                  control={
+                    <Checkbox
+                      checked={checked[0] && checked[1]}
+                      indeterminate={checked[0] !== checked[1]}
+                      onChange={handleChange1}
+                    />
+                  }
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'row', ml: 3 }}>
+                  <FormControlLabel
+                    label="USA"
+                    control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
+                    sx={{ border: '1px solid gray', borderRadius: '20px', pr: '10px' }}
+                  />
+                  <FormControlLabel
+                    label="UK"
+                    control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+                    sx={{ border: '1px solid gray', borderRadius: '20px', pr: '10px' }}
+                  />
+                  <FormControlLabel
+                    label="India"
+                    control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
+                    sx={{ border: '1px solid gray', borderRadius: '20px', pr: '10px' }}
+                  />
+                </Box>
+              </div>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <FormControl>
+          <FormLabel id="demo-row-radio-buttons-group-label">Publisher Network</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+          >
+            <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+            <FormControlLabel value="No" control={<Radio />} label="No" />
+            <FormControlLabel value="Only" control={<Radio />} label="Only" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="outlined-basic">Publisher Views</FormLabel>
+          <TextField id="outlined-basic" variant="outlined" type="number" size="small" />
+        </FormControl>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <FormControl sx={{}}>
+          <FormLabel>Select Ads Text</FormLabel>
+          <ToggleButtonGroup
+            color="primary"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+            // sx={{ width: '50%' }}
+          >
+            <ToggleButton value="installed">Installed</ToggleButton>
+            <ToggleButton value="uninstalled">Uninstalled</ToggleButton>
+          </ToggleButtonGroup>
+        </FormControl>
+        <FormControl sx={{ width: '40%' }}>
+          <FormLabel>Show Sites With</FormLabel>
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              type="number"
+              // size="small"
+              label="Min"
+              // sx={{ width: '10%' }}
+            />
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              type="number"
+              // size="small"
+              label="Max"
+              // sx={{ width: '10%' }}
+            />
+          </div>
+        </FormControl>
+      </div>
+      <FormGroup row sx={{ display: 'flex', justifyContent: 'center' }}>
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Keyword Search" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Has Articles" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Has Infinite Scroll" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Ignore DNS Server Sites" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Support AMP" />
+      </FormGroup>
     </div>
   );
 }
